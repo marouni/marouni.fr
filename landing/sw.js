44991fd31af4c9ed2cf1847cdefe29ba21ba7a55
abbass <abbass.marouni@gmail.com>
2018-12-03 22:04:03 +0100

    add service worker

---
+++ landing/sw.js
@@ -0,0 +1,23 @@
+const version = "0.1.1";
+const cacheName = `marouni-fr-${version}`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                `/`,
+                `/index.html`,
+                `/css/custom01.css`
+            ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
